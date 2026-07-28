33d500207ad76c3e5fc812477baf641a341c38b8
pipidastr001 <juliantop4ik@gmail.com>
2026-07-28 21:48:43 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,60 +1,60 @@
 from telebot import types
 
 def start_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Создать анкету"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Создать анкету"))
+    return k
 
 def gender_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("М"), types.KeyboardButton("Ж"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("М"), types.KeyboardButton("Ж"))
+    return k
 
 def main_menu_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Рейтить"), types.KeyboardButton("Моя анкета"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Рейтить"), types.KeyboardButton("Моя анкета"))
+    return k
 
 def my_profile_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Назад"))
-    keyboard.add(types.KeyboardButton("Изменить анкету"), types.KeyboardButton("Удалить анкету"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Назад"))
+    k.add(types.KeyboardButton("Изменить анкету"), types.KeyboardButton("Удалить анкету"))
+    return k
 
 def rating_keyboard(gender):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    
-    if gender == "M":
-        ratings = ["Sub 3", "Sub 5", "LTN", "MTN", "HTN", "Chad", "True Adam"]
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    if str(gender) == "M":
+        for r in ["Sub 3", "Sub 5", "LTN", "MTN", "HTN", "Chad", "True Adam"]:
+            k.add(types.KeyboardButton(r))
     else:
-        ratings = ["Sub 3", "Sub 5", "LTB", "MTB", "HTB", "Stacy", "True Eve"]
-    
-    for rating in ratings:
-        keyboard.add(types.KeyboardButton(rating))
-    
-    keyboard.add(types.KeyboardButton("Назад"))
-    
-    return keyboard
+        for r in ["Sub 3", "Sub 5", "LTB", "MTB", "HTB", "Stacy", "True Eve"]:
+            k.add(types.KeyboardButton(r))
+    k.add(types.KeyboardButton("Назад"))
+    return k
 
 def name_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Взять из Telegram"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Взять из Telegram"))
+    return k
 
 def done_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Готово"))
-    return keyboard
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Готово"))
+    return k
+
+def desc_keyboard():
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Готово"), types.KeyboardButton("Пропустить"))
+    return k
+
+def ad_keyboard():
+    k = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    k.add(types.KeyboardButton("Дальше"), types.KeyboardButton("Назад"))
+    return k
 
 def notification_keyboard():
-    keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(
-        types.InlineKeyboardButton("Запросить общение", callback_data="request_chat")
-    )
-    keyboard.add(
-        types.InlineKeyboardButton("Дальше", callback_data="next_rating")
-    )
-    keyboard.add(
-        types.InlineKeyboardButton("Пропустить всех", callback_data="skip_all")
-    )
-    return keyboard
+    k = types.InlineKeyboardMarkup()
+    k.add(types.InlineKeyboardButton("Запросить общение", callback_data="request_chat"))
+    k.add(types.InlineKeyboardButton("Дальше", callback_data="next_rating"))
+    k.add(types.InlineKeyboardButton("Пропустить всех", callback_data="skip_all"))
+    return k
