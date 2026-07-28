f2d0667dc7f011284d696d7570ec31252b02c862
pipidastr001 <juliantop4ik@gmail.com>
2026-07-28 04:21:17 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -10,16 +10,6 @@ def gender_keyboard():
     keyboard.add(types.KeyboardButton("М"), types.KeyboardButton("Ж"))
     return keyboard
 
-def photos_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Готово"))
-    return keyboard
-
-def name_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Взять из Telegram"))
-    return keyboard
-
 def main_menu_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(types.KeyboardButton("Рейтить"), types.KeyboardButton("Моя анкета"))
@@ -34,7 +24,7 @@ def my_profile_keyboard():
 def rating_keyboard(gender):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     
-    if gender == "M" or gender == "М":
+    if gender == "M":
         ratings = ["Sub 3", "Sub 5", "LTN", "MTN", "HTN", "Chad", "True Adam"]
     else:
         ratings = ["Sub 3", "Sub 5", "LTB", "MTB", "HTB", "Stacy", "True Eve"]
@@ -42,12 +32,27 @@ def rating_keyboard(gender):
     for rating in ratings:
         keyboard.add(types.KeyboardButton(rating))
     
+    return keyboard
+
+def back_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(types.KeyboardButton("Назад"))
+    return keyboard
+
+def done_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("Готово"))
     return keyboard
 
 def notification_keyboard():
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton("Запросить общение", callback_data="request_chat"))
-    keyboard.add(types.InlineKeyboardButton("Дальше", callback_data="next_rating"))
-    keyboard.add(types.InlineKeyboardButton("Пропустить всех", callback_data="skip_all"))
+    keyboard.add(
+        types.InlineKeyboardButton("Запросить общение", callback_data="request_chat")
+    )
+    keyboard.add(
+        types.InlineKeyboardButton("Дальше", callback_data="next_rating")
+    )
+    keyboard.add(
+        types.InlineKeyboardButton("Пропустить всех", callback_data="skip_all")
+    )
     return keyboard
