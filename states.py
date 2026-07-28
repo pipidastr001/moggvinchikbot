998d28527efcc64759e23cb55f999699a49a00e5
pipidastr001 <juliantop4ik@gmail.com>
2026-07-28 04:29:03 +0300

    Update states.py

--- states.py
+++ states.py
@@ -3,4 +3,3 @@ from telebot.handler_backends import State, StatesGroup
 class RegistrationStates(StatesGroup):
     waiting_for_gender = State()
     waiting_for_photos = State()
-    waiting_for_name = State()
