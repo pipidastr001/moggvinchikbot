26c56566e9aa8a6c54bda97b516c216d39dbff62
pipidastr001 <juliantop4ik@gmail.com>
2026-07-28 19:19:29 +0300

    Update ratings.py

--- ratings.py
+++ ratings.py
@@ -1,4 +1,19 @@
 from database import db
+import random
+
+def get_all_users_for_rating(current_user_id):
+    users = db.get_all_active_users(exclude_user_id=current_user_id)
+    random.shuffle(users)
+    return users
+
+user_queues = {}
+
+def get_queue_for_user(user_id):
+    # Всегда создаём новую очередь чтобы видеть все анкеты
+    queue = RatingQueue()
+    queue.users_pool = get_all_users_for_rating(user_id)
+    queue.current_index = 0
+    return queue
 
 class RatingQueue:
     def __init__(self):
@@ -7,7 +22,7 @@ class RatingQueue:
     
     def get_next_user(self, current_user_id):
         if not self.users_pool or self.current_index >= len(self.users_pool):
-            self.users_pool = db.get_all_active_users(exclude_user_id=current_user_id)
+            self.users_pool = get_all_users_for_rating(current_user_id)
             self.current_index = 0
         
         if self.users_pool:
@@ -15,10 +30,3 @@ class RatingQueue:
             self.current_index += 1
             return user
         return None
-
-user_queues = {}
-
-def get_queue_for_user(user_id):
-    if user_id not in user_queues:
-        user_queues[user_id] = RatingQueue()
-    return user_queues[user_id]
